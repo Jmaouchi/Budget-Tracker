59028060b1c0fed61406c89b94f204857e16c3e0
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-06-11 21:17:54 -0500

    added the activate methode

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,3 +19,44 @@ self.addEventListener('install', function (e) {
 })
 
 
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) { //.keys() returns an array of all cache names, which we're calling keyList. 
+      //keyList is a parameter that contains all cache names under <username>.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+// retrieve informations from the cache
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
